e51d163da5478a6aa4cb20190534357ec1c2bc21
RLimbaugh <50933296+RLimbaugh@users.noreply.github.com>
2019-05-23 12:05:24 -0400

    changed stuff

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template, url_for
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello World'
+    return render_template('home.html', title='Hello')
+
+@app.route('/registration')
+def registration():
+  form = RegistrationForm()
+  return render_template('registration.html', form=form)
