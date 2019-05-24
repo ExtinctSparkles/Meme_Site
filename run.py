18c1d51c6bd890903f2f2fde8eb9b251c79654ad
RLimbaugh <50933296+RLimbaugh@users.noreply.github.com>
2019-05-24 09:21:06 -0400

    added comment so we can paste it into main.py

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+'''
+from app import app
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
+'''
