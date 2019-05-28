88b0eadafe35e0fde289528f1abd4df31fbc67c4
ExtinctSparkles <brennanMJ123@gmail.com>
2019-05-28 09:36:52 -0400

    Comment to change an import so it will run on repl

--- run.py
+++ run.py
@@ -8,3 +8,4 @@ from app import app
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
 '''
+'''Make sure you go into routes and change the secrets import to secret so the website will run'''
