547903c60fa0d207dc9bfc58a6630e4acc91fafa
ExtinctSparkles <brennanMJ123@gmail.com>
2019-05-28 09:40:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
 '''
 '''Make sure you go into routes and change the secrets import to secret so the website will run'''
+'''Test User is Username:testUser password:123'''
