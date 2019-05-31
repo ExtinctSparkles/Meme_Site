9f92913a4d7cb17b0f11e226c7e99f73914de91c
Brennan Johnson <brennanMJ123@gmail.com>
2019-05-30 20:12:15 -0400

    database issue fixed

--- app/models.py
+++ app/models.py
@@ -9,9 +9,9 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
-    image = db.Column(db.String(120), nullable=False, default='test_pic.jpg')
+    image = db.Column(db.String(120), default='test_pic.jpg')
 
     def __repr__(self):
         return "User({}, {}, {})".format(self.id, self.username, self.image)
@@ -21,7 +21,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(120))
     body = db.Column(db.String(120))
-    likes = db.Column(db.Integer, nullable=False, default=0)
+    likes = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return "Post({}, {}, {})".format(self.image, self.body, self.likes)
